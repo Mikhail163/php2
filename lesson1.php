bb076e106196138424616e2aa172344f20d3dd2a
MistralB <mephist0_87@mail.ru>
2018-08-28 22:21:34 +0400

    lesson1

--- lesson1.php
+++ lesson1.php
@@ -1,4 +1,172 @@
 <?php
+/**
+ * Урок 1 PHP 2
+ * @author Уваров Михаил
+ *
+ */
+
+class Book extends Product {
+	public $mAuthor = '';
+	
+	public function __construct($name, $price, $author, $page_number) {
+		parent::__construct($name, $age);
+		
+		$this->mAuthor = $author;
+		
+		parent::$mState['page_number'] = 0;
+		
+		parent::state('page_number', $page_number);
+	}
+}
+
+class Product {
+	
+	protected $mName;
+	public $mDescription;
+	
+	protected $mState = [
+			'retail_price' => 0,
+			'trade_price'  => 0,
+			'lenght' => 0,
+			'height' => 0,
+			'width'  => 0,
+			'weight' => 0
+	];
+	
+	public function __construct($name, $price) {
+		
+		if (empty($name))
+			throw new Exception("Имя товара не может иметь пустое значение");
+			
+			$this->mName = $name;
+			$this->price($price);
+			
+	}
+	
+	public function get() {
+		
+		$product = "Название: {$this->mName}; описание: {$this->mDescription};";
+		
+		foreach ($this->mState as $key => $value) {
+			$product .= " {$key}: {$value};";
+		}
+		
+		return $product;
+	}
+	
+	public function view() {
+		echo $this->get();
+	}
+	
+	public function name($name = '') {
+		
+		if (empty($name))
+			return $this->mName;
+			else
+				$this->mName = $name;
+	}
+	
+	
+	public function price($price=-1) {
+		
+		return $this->state('retail_price', $price);
+		
+	}
+	
+	public function tradePrice($price=-1) {
+		
+		return $this->state('trade_price', $price);
+		
+	}
+	
+	public function lenght($lenght=-1) {
+		
+		return $this->state('lenght', $price);
+		
+	}
+	
+	public function height($height=-1) {
+		
+		return $this->state('height', $price);
+		
+	}
+	
+	public function width($width=-1) {
+		
+		return $this->state('width', $price);
+		
+	}
+	
+	public function weight($weight=-1) {
+		
+		return $this->state('weight', $price);
+		
+	}
+	
+	private function state($state_name = '', $val = -1) {
+		
+		if (!empty($state_name)) {
+			
+			if (isset($this->mState[$state_name])) {
+				if ($val < 0 || !is_numeric($val))
+					return $this->mState[$state_name];
+					else
+						$this->mState[$state_name] = $val;
+			}
+			else {
+				throw new Exception("{$state_name} Неизвестное свойство.");
+			}
+			
+		}
+		else
+			throw new Exception('Не указано название свойства.');
+	}
+	
+}
+
+class A {
+	//static $x = 100;
+	public function foo() {
+		static $x = 0;
+		echo ++$x;
+	}
+}
+
+/*
+ * Поскольку переменная $x статична в классе
+ * то во всех классах это ссылка на один и тот же
+ * участок памяти, где хранится переменная
+ * Поэтому все классы будут обращаться к одной и той же переменной
+ */
+
+$a1 = new A();
+$a2 = new A();
+$a1->foo();
+$a2->foo();
+$a1->foo();
+$a2->foo();
+
+/*
+ * undeclared static property действуют только в пределах одного класса
+ * если класс изменяется - то соответсвенно undeclared static property
+ * будут лежать в другом участке памяти
+ */
+
+class B extends A {
+	public function foo2() {
+		echo ++parent::$x;
+	}
+	
+}
+$a1 = new A;
+$b1 = new B;
+$a1->foo();
+$b1->foo();
+$a1->foo();
+$b1->foo(); 
+$b1->foo2(); 
+$b1->foo2(); 
+
 
 
 class Book extends Product {
@@ -64,12 +232,6 @@ class Product {
 	
 	
 	public function price($price=-1) {
-		
-		return $this->state('retail_price', $price);
-		
-	}
-	
-	public function price($price=-1) {		
 		
 		return $this->state('retail_price', $price);
 		
